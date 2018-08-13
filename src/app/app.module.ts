e19bb3cae242acd82369267e6e76cdcf97c72ae4
ersimarpreetsingh <simarpreet@cybertrontechnologies.com>
2018-08-13 19:23:53 +0530

    small update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
+import { WelcomePage } from './../pages/welcome/welcome';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    LoginPage,
+    WelcomePage
   ],
   imports: [
     BrowserModule,
@@ -22,8 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    LoginPage,
+    WelcomePage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,33 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+
+/**
+ * Generated class for the WelcomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html',
+})
+export class WelcomePage {
+  @ViewChild(Slides) slides: Slides;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad WelcomePage');
+    this.slides.lockSwipes(true);
+  }
+
+  nextSlide(){
+    this.slides.lockSwipes(false);
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
+  }
+}
