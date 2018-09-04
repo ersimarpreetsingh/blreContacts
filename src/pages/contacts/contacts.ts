3b84612faf73ae547886b7ca5bdbb3e33e66bbf2
ashishkumarsatyam <ashish@cybertrontechnologies.com>
2018-09-04 17:05:51 +0530

    changes done

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import * as faker from 'faker';
 /**
  * Generated class for the ContactsPage page.
  *
@@ -14,14 +14,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'contacts.html',
 })
 export class ContactsPage {
+  public userArray:any = [];
+  public prevValue:any = null
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.userArray= [];
+    for(let i=0; i<30; i++){
+      this.userArray.push(
+                    {  
+                      firstName: faker.name.firstName(), 
+                      lastName: faker.name.lastName(),
+                      fullName: faker.name.findName(),
+                    }
+      );
+    }
+    this.userArray.sort(function(a, b) {
+      var nameA = a.firstName.toUpperCase();
+      var nameB = b.firstName.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactsPage');
   }
+
   importContacts(){
     this.navCtrl.push('ImportContactsPage');
+  }
+
+  getValue(val){
+    if(this.prevValue == null){
+      this.prevValue = val.charAt(0);
+      return val.charAt(0);
+    }
+    else if(this.prevValue == val.charAt(0)){
+      return null;
+    }
+    else{
+      this.prevValue = val.charAt(0);
+      return val.charAt(0);
+    } 
+  }
+
+  contactDetailsPage(contact){
+    this.navCtrl.push('ViewContactPage', contact);
   }
 }
